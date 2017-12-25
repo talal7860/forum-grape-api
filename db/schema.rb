11f59f078422117c40f6b6d737a550363ef0a74d
Talal Arshad <talal7860@gmail.com>
2017-12-26 00:29:02 +0500

    rspec install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223093613) do
+ActiveRecord::Schema.define(version: 20171225154407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 20171223093613) do
     t.datetime "updated_at", null: false
     t.index ["added_by_id"], name: "index_topics_on_added_by_id"
     t.index ["forum_id"], name: "index_topics_on_forum_id"
+  end
+
+  create_table "user_tokens", force: :cascade do |t|
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
