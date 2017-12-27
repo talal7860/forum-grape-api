8cabdfd2b8f31788fe9e109bc27d00650a8bfb3c
Talal Arshad <talal7860@gmail.com>
2017-12-28 01:58:41 +0500

    adding slug to topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,11 @@
 class Topic < ApplicationRecord
   belongs_to :forum
   belongs_to :added_by, class_name: 'User', foreign_key: :added_by_id
+  after_create :create_slug
+
+  private
+  def create_slug
+    slug = "#{self.title} #{self.id}".parameterize
+    self.update_columns(slug: slug)
+  end
 end
--- db/migrate/20171221212405_create_topics.rb
+++ db/migrate/20171221212405_create_topics.rb
@@ -4,6 +4,7 @@ class CreateTopics < ActiveRecord::Migration[5.1]
       t.string :title
       t.integer :added_by_id, index: true
       t.integer :forum_id, index: true
+      t.string :slug, index: true
 
       t.timestamps
     end
