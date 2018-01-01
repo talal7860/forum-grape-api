27e2c58c49aa0dc8bb4d4eb20dfcc4c5f1b99dde
Talal Arshad <talal7860@gmail.com>
2018-01-02 02:18:01 +0500

    adding back the v1 folder downcased

---
+++ spec/api/v1/sessions_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe ApplicationApi::V1::Sessions do
+  include Rack::Test::Methods
+
+  def app
+    ApplicationApi
+  end
+
+  describe 'POST /api/sessions' do
+    let(:user) { FactoryBot.create(:user, email: 'customer@customer.com') }
+    let (:body) do
+      {
+        email: 'customer@customer.com',
+        password: 'password'
+      }
+    end
+
+    it 'creates a token for logging in' do
+      user
+      post '/api/sessions', body, { 'Content-Type' => 'application/json' }
+
+      expect(last_response.status).to eq(201)
+      expect(JSON.parse(last_response.body)['data']['token']).to eq(UserToken.first.token)
+    end
+
+    it 'does not create a token for invalid user name password' do
+      user
+      post '/api/sessions', {email: 'customer@customer.com', password: 'asdf'}, { 'Content-Type' => 'application/json' }
+
+      expect(last_response.status).to eq(401)
+    end
+  end
+
+  describe 'DELETE /api/sessions' do
+    let(:user) { FactoryBot.create(:user, email: 'customer@customer.com') }
+
+    it 'deletes a valid session' do
+      user.login!
+      header 'Content-Type', 'application/json'
+      header 'Authorization', user.user_tokens.first.token
+      delete '/api/sessions'
+
+      expect(last_response.status).to eq(200)
+      expect(user.user_tokens.count).to eq(0)
+    end
+
+    it 'does not delete an invalid session' do
+      header 'Content-Type', 'application/json'
+      header 'Authorization', 'invalid session'
+      delete '/api/sessions'
+
+      expect(last_response.status).to eq(401)
+    end
+  end
+
+end
